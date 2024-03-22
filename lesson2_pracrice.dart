599da52a98b264cd98f98073fe1d5596375f5179
mirlanraemkulov00 <strategarxy@gmail.com>
2024-03-22 18:50:46 +0500

    test

---
+++ lesson2_pracrice.dart
@@ -0,0 +1,101 @@
+// ignore_for_file: unused_local_variable
+
+void main (){
+num b = 7;
+print(b);
+num q = 11;
+print(q);
+num e = 25;
+print(e);
+num t = 9.03;
+print(t);
+num y = 79.99;
+print(y);
+
+int a = 3;
+print(a);
+int k = 19;
+print(k);
+int l = 22;
+print(l);
+int h = 55;
+print(h);
+int d = 40;
+print(d);
+
+double x = 12.3;
+print(x);
+double c = 7.05;
+print(c);
+double m = 21.41;
+print(m);
+double v = 41.1;
+print(v);
+double u = 31.74;
+print(u);
+
+String name = "Акжол";
+print(name);
+String name = "Бексултан";
+print(name);
+String name = "Тимур";
+print(name);
+String name = "Сезим";
+print(name);
+String name = "Айтурган";
+print(name);
+
+List array = [999];
+print(array);
+
+List array = [true];
+print(array);
+
+List array = ["foot","book","window"];
+print(array);
+
+List array = [65.06, 956.32, 45.21];
+print(array);
+
+List array = [1344,2154,2125]
+print(array);
+
+bool z = true;
+bool p = true;
+bool j = true;
+bool f = true;
+bool g = true;
+
+Map user = {
+"name" : "Tesla",
+"age" : "31",
+"a" : true,
+};
+Map user = {
+"name" : "John",
+"age" : "28",
+"d" : true,
+};
+Map user = {
+"name" : "Ernis",
+"age" : "21",
+"l" : true,
+};
+Map user = {
+"name" : "Damir",
+"age" : "18",
+"" : true,
+};
+Map user = {
+"name" : "Beka",
+"age" : "18",
+"r" : true,
+};
+final int test7 = 15;
+print(test7);
+test7 = 5;
+
+final int studio 
+
+
+}
